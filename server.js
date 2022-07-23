5e0b994876ebea67eb11fd6a37eea7c7875edbbe
unknown <tavoywalls@gmail.com>
2022-07-23 10:16:04 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 
 
 // ROUTES
@@ -19,9 +21,9 @@ app.get('/', (req, res) => {
 })
 
 // 404
-app.get('*', (req, res) => {
-  res.send('404')
-})
+// app.get('*', (req, res) => {
+//   res.send('404')
+// })
 
 //BREADS
 const breadsController = require('./controllers/breads_controller.js')
--- views/index.jsx
+++ views/index.jsx
@@ -1,24 +1,27 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index({breads, title}) {
+function Index({ breads, title }) {
     return (
         <Default title={title}>
             <h2>Index Page</h2>
             {/* <p>I have {breads[0].name} bread!</p> */}
             <ul>
                 {
-                    breads.map((bread, index)=> {
+                    breads.map((bread, index) => {
                         return (
-                        <li key={index}>
-                            <a href={`/breads/${index}`}>
-                                {bread.name}
-                            </a>
-                        </li>
+                            <li key={index}>
+                                <a href={`/breads/${index}`}>
+                                    {bread.name}
+                                </a>
+                            </li>
                         )
                     })
                 }
             </ul>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
         </Default>
     )
 }
